446cc543c9872d18ca933f6c8fe28f78591cfb85
lhenne <lhenne@posteo.de>
2020-06-05 17:28:27 +0200

    add function to represent transcript as DataFrame

---
+++ prep/prepare_transcriptions.py
@@ -0,0 +1,23 @@
+""" 
+Import necessary packages:
+`re` for regular expressions
+`pandas` for providing a data structure to store the CSV data in within Python
+`StringIO` to provide an object for pandas functions to parse
+"""
+
+import re
+import pandas as pd
+from io import StringIO
+
+def getTranscriptData(transcriptFile):
+
+    with open(transcriptFile, "r", encoding="iso-8859-1") as f:
+        while f.readline() != "#\n": # A lone hash sign in a line signifies the start of the data block
+            pass
+        rawContent = f.read()
+    
+    rawContent = rawContent.replace("    ", "") # Eliminate the indent before each line
+    content = StringIO(rawContent) # Convert to a StringIO object that can be parsed by the `pandas.read_csv` function
+    transcriptData = pd.read_csv(content, sep=" ", engine="python" , quoting=3)
+   
+    return transcriptData
