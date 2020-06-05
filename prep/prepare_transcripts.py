d63c425c91cc03bdb1f40d2f96299b71e07a7a3f
lhenne <lhenne@posteo.de>
2020-06-05 20:23:17 +0200

    change function name, name DataFrame columns

--- prep/prepare_transcripts.py
+++ prep/prepare_transcripts.py
@@ -9,15 +9,17 @@ import re
 import pandas as pd
 from io import StringIO
 
-def getTranscriptData(transcriptFile):
+
+def get_transcript_data(transcriptFile):
 
     with open(transcriptFile, "r", encoding="iso-8859-1") as f:
         while f.readline() != "#\n": # A lone hash sign in a line signifies the start of the data block
             pass
         rawContent = f.read()
     
-    rawContent = rawContent.replace("    ", "") # Eliminate the indent before each line
+    rawContent = re.sub("[ ]{3,}", "", rawContent).lstrip() # Eliminate the indent before each line
+
     content = StringIO(rawContent) # Convert to a StringIO object that can be parsed by the `pandas.read_csv` function
-    transcriptData = pd.read_csv(content, sep=" ", engine="python" , quoting=3)
-   
+    transcriptData = pd.read_csv(content, sep=" ", engine="python" , quoting=3, names=["start","xwaves","label"])
+
     return transcriptData
