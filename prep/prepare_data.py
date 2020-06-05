e60725acfffbb44dbe3afc8469bb74ca6fec68dc
lhenne <lhenne@posteo.de>
2020-06-05 17:19:52 +0200

    set up group of scripts to prepare DIRNDL data

---
+++ prep/prepare_data.py
@@ -0,0 +1,12 @@
+""" 
+The functions in this module reformat the data from the DIRNDL corpus in order to later be able to make use of them for the acoustic analysis.
+"""
+
+import prepare_transcriptions
+import prepare_tones
+import prepare_accents
+import get_speaker_info
+
+class DataPreparation(corpusPath, recordingID):
+    def __init__(self):
+        return None
