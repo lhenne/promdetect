1569de47dd74045d85e0d2d7b05645413551ce08
lhenne <lhenne@posteo.de>
2020-06-06 11:53:03 +0200

    add function to reformat tone annotations

--- prep/prepare_data.py
+++ prep/prepare_data.py
@@ -12,19 +12,20 @@ class DataPreparation:
 
     def __init__(self, corpusPath, recordingID):
 
-        # assign all relevant file paths to their corresponding variables
+        # Assign all relevant file paths to their corresponding variables
         self.transcriptFile = "{0}/dlf-nachrichten-{1}.words".format(corpusPath, recordingID)
         self.tonesFile = "{0}/dlf-nachrichten-{1}.tones".format(corpusPath, recordingID)
         self.accentsFile = "{0}/dlf-nachrichten-{1}.accents".format(corpusPath, recordingID)
 
-        # get gender and ID of speaker
+        # Get gender and ID of speaker
         self.speakerGender = get_speaker_info.get_gender(recordingID)
         self.speakerID = get_speaker_info.get_id(recordingID)
     
     def transform_data(self):
         
         self.transcript = prepare_transcripts.get_transcript_data(self.transcriptFile)
-
+        self.tones = prepare_tones.get_tones_data(self.tonesFile)
+        a = 0
 
 
 a = DataPreparation("/home/lukas/Dokumente/Uni/ma_thesis/quelldaten/DIRNDL-prosody", "200703271500")
