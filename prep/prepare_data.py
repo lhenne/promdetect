68b797e6ed403285bee88a18e706883be9574882
lhenne <lhenne@posteo.de>
2020-06-06 10:33:27 +0200

    add `transform_data` to call reformat functions

--- prep/prepare_data.py
+++ prep/prepare_data.py
@@ -2,7 +2,7 @@
 The functions in this module reformat the data from the DIRNDL corpus in order to later be able to make use of them for the acoustic analysis.
 """
 
-import prepare_transcriptions
+import prepare_transcripts
 import prepare_tones
 import prepare_accents
 import get_speaker_info
@@ -11,12 +11,21 @@ class DataPreparation:
     
 
     def __init__(self, corpusPath, recordingID):
+
         # assign all relevant file paths to their corresponding variables
         self.transcriptFile = "{0}/dlf-nachrichten-{1}.words".format(corpusPath, recordingID)
         self.tonesFile = "{0}/dlf-nachrichten-{1}.tones".format(corpusPath, recordingID)
         self.accentsFile = "{0}/dlf-nachrichten-{1}.accents".format(corpusPath, recordingID)
 
-        # get gender of speaker
+        # get gender and ID of speaker
         self.speakerGender = get_speaker_info.get_gender(recordingID)
+        # self.speakerID = get_speaker_info.get_id(recordingID) # TODO: create this function in get_speaker_info
+    
+    def transform_data(self):
+        
+        self.transcript = prepare_transcripts.get_transcript_data(self.transcriptFile)
 
-a = DataPreparation("~/Dokumente/Uni/ma_thesis/quelldaten/DIRNDL-prosody", "200703271500")
+
+
+a = DataPreparation("/home/lukas/Dokumente/Uni/ma_thesis/quelldaten/DIRNDL-prosody", "200703271500")
+a.transform_data()
