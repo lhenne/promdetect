2524bd53b19023080a37bb8501a8145909167622
lhenne <lhenne@posteo.de>
2020-06-06 10:45:07 +0200

    add explanatory comments

--- prep/get_speaker_info.py
+++ prep/get_speaker_info.py
@@ -1,11 +1,13 @@
 import re
 
+#### Get the gender of a speaker from their ID
+
 def get_gender(recordingID):
 
-    with open("/home/lukas/Dokumente/Uni/ma_thesis/quelldaten/DIRNDL-prosody/speakers-prosodically-annotated-part.txt", "r") as speakerFile:
+    with open("/home/lukas/Dokumente/Uni/ma_thesis/quelldaten/DIRNDL-prosody/speakers-prosodically-annotated-part.txt", "r") as speakerFile: # Use speaker info text file
         for line in speakerFile:
             if recordingID in line:
-                speakerGender = re.sub(r"[^a-z]*", "", line)
+                speakerGender = re.sub(r"[^a-z]*", "", line) # Clean all unnecessary characters from the line where the ID was found
                 break
         if speakerGender == "m":
             return "male"
