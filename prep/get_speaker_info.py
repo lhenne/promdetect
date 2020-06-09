8c19e471ce0d71cd684a1c576d7aac82f917e380
lhenne <lhenne@posteo.de>
2020-06-09 19:12:36 +0200

    update code commentary

--- prep/get_speaker_info.py
+++ prep/get_speaker_info.py
@@ -1,3 +1,8 @@
+"""
+Import necessary packages:
+`re` for regular expressions
+"""
+
 import re
 
 
@@ -5,12 +10,16 @@ import re
 
 def get_gender(recordingID):
 
-    with open("/home/lukas/Dokumente/Uni/ma_thesis/quelldaten/DIRNDL-prosody/speakers-prosodically-annotated-part.txt", "r") as speakerFile:  # Use speaker info text file
+    # Use speaker info text file to collect all data
+    with open("/home/lukas/Dokumente/Uni/ma_thesis/quelldaten/DIRNDL-prosody/speakers-prosodically-annotated-part.txt", "r") as speakerFile:
+        # Search by line if it includes the current recording's ID
         for line in speakerFile:
             if recordingID in line:
                 # Clean all unnecessary characters from the line where the ID was found
                 speakerGender = re.sub(r"[^a-z]*", "", line)
                 break
+        # After cleaning, only the last letter in the line remains, which indicates the speaker gender.
+        # If the format is not as expected, throw an error
         if speakerGender == "m":
             return "male"
         elif speakerGender == "f":
@@ -24,8 +33,11 @@ def get_gender(recordingID):
 
 def get_id(recordingID):
 
-    with open("/home/lukas/Dokumente/Uni/ma_thesis/quelldaten/DIRNDL-prosody/speakers-prosodically-annotated-part.txt", "r") as speakerFile:  # Use speaker info text file
+    # Use speaker info text file to collect all data
+    with open("/home/lukas/Dokumente/Uni/ma_thesis/quelldaten/DIRNDL-prosody/speakers-prosodically-annotated-part.txt", "r") as speakerFile:
+        # Search by line if it includes the current recording's ID
         for line in speakerFile:
             if recordingID in line:
+                # Extract the ID (a number) of the speaker
                 speakerID = re.findall(r"[0-9 ]+SP([0-9]?)[fm]", line)[0]
     return speakerID
