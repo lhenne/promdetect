1c51a83e6435cfa99d66c23ea31cdebeaca5612d
lhenne <lhenne@posteo.de>
2020-06-09 18:59:44 +0200

    add code commentary

--- prep/prepare_transcripts.py
+++ prep/prepare_transcripts.py
@@ -12,13 +12,20 @@ from io import StringIO
 
 def get_transcript_data(transcriptFile):
 
+    # Use ISO 8859-1 (Western European) encoding to open TSV file containing transcript
     with open(transcriptFile, "r", encoding="iso-8859-1") as f:
+        # Don't save any lines until a lone hash sign is found in the line
+        # This signifies the end of the metadata and start of the data block
         while f.readline() != "#\n":
             pass
         rawContent = f.read()
 
+    # Clean data and replace escaped Umlaute with proper ones by running `clean_text` function
+    # Convert data to a StringIO object that can be parsed by the `pandas.read_csv` function
     content = StringIO(clean_text(rawContent))
 
+    # Read the data into a pandas DataFrame
+    # Drop empty column 'idx' directly (caused by a leading whitespace in each line)
     transcriptData = pd.read_csv(content,
                                  sep=" ",
                                  engine="python",
@@ -28,8 +35,12 @@ def get_transcript_data(transcriptFile):
                                         "xwaves",
                                         "label"]).drop(["idx"], axis=1)
 
+    # Create an empty column to which the start timestamps for each word will be assigned
     transcriptData["start"] = None
 
+    # Iterate over the rows of transcriptData, and use the 'end' timestamp from the preceding row
+    # to derive the 'start' timestamp for the current one.
+    # The 'start' timestamp is estimated by adding 10 milliseconds to the 'end' timestamp of the previous word
     for i in transcriptData.index:
         if i <= len(transcriptData.index) and i > 0:
             transcriptData.at[i, "start"] = transcriptData.loc[i - 1,
@@ -44,9 +55,10 @@ def get_transcript_data(transcriptFile):
 
 
 def clean_text(rawText):
+    # Reduce all instances two or more consecutive whitespaces to one whitespace
     text = re.sub(r" {2,}", " ", rawText)
 
-    #  Fix Umlaute and scharfes S
+    #  Replace escaped Umlaut with proper ones
     replacements = {
         "\"u": "ü",
         "\"U": "Ü",
