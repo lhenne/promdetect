29eb7f9b782831ccb6f146eb1553aeeefb535d1b
lhenne <lhenne@posteo.de>
2020-06-12 17:42:31 +0200

    unite annotation processing functions into one

--- prep/process_annotations.py
+++ prep/process_annotations.py
@@ -10,76 +10,49 @@ import pandas as pd
 from io import StringIO
 
 
-def get_accents_data(accentsFile):
+def get_annotation_data(annotationFile, annotationType):
 
     # Read data from the input file starting from specific marker
-    rawContent = read_file(accentsFile)
+    rawContent = read_file(annotationFile)
 
     # Clean data and convert to a StringIO object that can be parsed by the `pandas.read_csv` function
-    content = clean_text(rawContent, "accents")
+    content = clean_text(rawContent, annotationType)
 
     # Read the data into a pandas DataFrame
     # Drop empty column 'idx' directly (caused by a leading whitespace in each line)
-    accentsData = pd.read_csv(content, sep=" ", engine="python", quoting=3, names=[
-                              "idx", "time", "xwaves", "label"]).drop(["idx"], axis=1)
+    if annotationType == "words":
+        annotationData = pd.read_csv(content,
+                                     sep=" ",
+                                     engine="python",
+                                     quoting=3,
+                                     names=["idx",
+                                            "end",
+                                            "xwaves",
+                                            "label"]).drop(["idx"], axis=1)
 
-    return accentsData
+        # Create an empty column to which the start timestamps for each word will be assigned
+        annotationData["start"] = None
 
+        # Iterate over the rows of annotationData, and use the 'end' timestamp from the preceding row
+        # to derive the 'start' timestamp for the current one.
+        # The 'start' timestamp is estimated by adding 10 milliseconds to the 'end' timestamp of the previous word
+        for i in annotationData.index:
+            if i <= len(annotationData.index) and i > 0:
+                annotationData.at[i, "start"] = annotationData.loc[i - 1,
+                                                                   "end"] + 0.001
+            elif i == 0:
+                annotationData.at[i, "start"] = 0
 
-def get_tones_data(tonesFile):
+    elif annotationType in ["tones", "accents"]:
+        # Read the data into a pandas DataFrame
+        # Drop empty column 'idx' directly (caused by a leading whitespace in each line)
+        annotationData = pd.read_csv(content, sep=" ", engine="python", quoting=3, names=[
+            "idx", "time", "xwaves", "label"]).drop(["idx"], axis=1)
 
-    # Read data from the input file starting from specific marker
-    rawContent = read_file(tonesFile)
-
-    # Clean data and convert to a StringIO object that can be parsed by the `pandas.read_csv` function
-    content = clean_text(rawContent, "tones")
-
-    # Read the data into a pandas DataFrame
-    # Drop empty column 'idx' directly (caused by a leading whitespace in each line)
-    tonesData = pd.read_csv(content, sep=" ", engine="python", quoting=3, names=[
-                            "idx", "time", "xwaves", "label"]).drop(["idx"], axis=1)
-
-    return tonesData
-
-
-def get_transcript_data(transcriptFile):
-
-    # Read data from the input file starting from specific marker
-    rawContent = read_file(transcriptFile)
-
-    # Clean data and replace escaped Umlaute with proper ones by running `clean_text` function
-    # Convert data to a StringIO object that can be parsed by the `pandas.read_csv` function
-    content = clean_text(rawContent, "words")
-
-    # Read the data into a pandas DataFrame
-    # Drop empty column 'idx' directly (caused by a leading whitespace in each line)
-    transcriptData = pd.read_csv(content,
-                                 sep=" ",
-                                 engine="python",
-                                 quoting=3,
-                                 names=["idx",
-                                        "end",
-                                        "xwaves",
-                                        "label"]).drop(["idx"], axis=1)
-
-    # Create an empty column to which the start timestamps for each word will be assigned
-    transcriptData["start"] = None
-
-    # Iterate over the rows of transcriptData, and use the 'end' timestamp from the preceding row
-    # to derive the 'start' timestamp for the current one.
-    # The 'start' timestamp is estimated by adding 10 milliseconds to the 'end' timestamp of the previous word
-    for i in transcriptData.index:
-        if i <= len(transcriptData.index) and i > 0:
-            transcriptData.at[i, "start"] = transcriptData.loc[i - 1,
-                                                               "end"] + 0.001
-        elif i == 0:
-            transcriptData.at[i, "start"] = 0
-
-    return transcriptData
+    return annotationData
 
 
 # Ancillary functions block
-
 
 def clean_text(rawText, annotationType):
     # Reduce all instances two or more consecutive whitespaces to one whitespace
