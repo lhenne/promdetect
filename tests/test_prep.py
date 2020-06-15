369699caaadb832ecfb691acf3cd4b5eaea99421
lhenne <lhenne@posteo.de>
2020-06-15 12:47:27 +0200

    add tests for `get_speaker_info.py`

--- tests/test_prep.py
+++ tests/test_prep.py
@@ -21,3 +21,20 @@ class TestFindSyllableNuclei(unittest.TestCase):
 
     def test_that_fun_fails_if_file_is_not_right_format(self):
         self.assertRaises(PraatError, syllable_nuclei.determine_nucleus_points, "lorem")
+
+
+class TestGetSpeakerInfo(unittest.TestCase):
+
+    def test_that_funs_output_correct_data_type(self):
+        self.assertIsInstance(speaker_info.get_gender("200703271500"), str)
+        self.assertIsInstance(speaker_info.get_id("200703271500"), str)
+
+    def test_that_funs_fail_when_input_is_wrong_type(self):
+        self.assertRaises(TypeError, speaker_info.get_gender, [42, 63])
+        self.assertRaises(TypeError, speaker_info.get_id, [52, 73])
+        self.assertRaises(TypeError, speaker_info.get_gender, list)
+        self.assertRaises(TypeError, speaker_info.get_id, str)
+
+    def test_that_funs_fail_when_input_is_not_found(self):
+        self.assertRaises(ValueError, speaker_info.get_gender, "20071234567")
+        self.assertRaises(ValueError, speaker_info.get_id, "20079876543")
