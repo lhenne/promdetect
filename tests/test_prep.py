941354d12aa0744ef15c00403d46981c6a52da66
lhenne <lhenne@posteo.de>
2020-09-22 11:03:28 +0200

    refactor: add tests for annotation import function

--- tests/test_prep.py
+++ tests/test_prep.py
@@ -12,6 +12,10 @@ class AnnotationImportTests(unittest.TestCase):
     """
 
     def test_read_file(self):
+        """
+        Test read_file(): Are files imported with the correct formatting?
+        """
+
         input_file = "/home/lukas/Dokumente/Uni/ma_thesis/promdetect/tests/test_material/test.accents"
 
         correct_output = "    17.748754  121 H*L\n    19.194136  121 H*\n\n"
@@ -19,11 +23,65 @@ class AnnotationImportTests(unittest.TestCase):
         self.assertEqual(process_annotations.read_file(input_file), correct_output)
 
     def test_clean_text(self):
+        """
+        Test clean_text(): Are strings reformatted and normalized correctly?
+        """
+
         input_text = '    das\n      k"onnte besser auss"ahen\n\n'
         annotation_type = "words"
 
-        correct_output = " das\n könnte besser aussähen\n\n"
+        correct_output = "das\n könnte besser aussähen\n\n"
 
         self.assertEqual(
             process_annotations.clean_text(input_text, annotation_type), correct_output
         )
+
+    def test_annotation_input_type(self):
+        """
+        Test get_annotation_data(): Do invalid input types produce an error?
+        """
+
+        annotation_file = "/home/lukas/Dokumente/Uni/ma_thesis/promdetect/tests/test_material/test.phrases"
+
+        with self.assertRaises(ValueError):
+            process_annotations.get_annotation_data(annotation_file)
+
+    def test_annotation_missing_file(self):
+        """
+        Test get_annotation_data(): Does an invalid file path produce an error?
+        """
+
+        annotation_file = "does/not/exist.accents"
+
+        with self.assertRaises(FileNotFoundError):
+            process_annotations.get_annotation_data(annotation_file)
+
+    def test_annotation_accents_output(self):
+        """
+        Test get_annotation_data(): Does the function return the correct output for a file containing accent labels?
+        """
+
+        annotation_file = "test_material/test.accents"
+
+        output_df = process_annotations.get_annotation_data(annotation_file)
+
+        self.assertTrue("time" in output_df.columns)
+        self.assertFalse("end" in output_df.columns)
+        self.assertFalse("xwaves" in output_df.columns)
+        self.assertTrue("label" in output_df.columns)
+        self.assertTrue(len(output_df.index) == 2)
+
+    def test_annotation_words_output(self):
+        """
+        Test get_annotation_data(): Does the function return the correct output for a file containing word labels?
+        """
+
+        annotation_file = "test_material/test.words"
+
+        output_df = process_annotations.get_annotation_data(annotation_file)
+
+        self.assertTrue("end" in output_df.columns)
+        self.assertFalse("time" in output_df.columns)
+        self.assertFalse("xwaves" in output_df.columns)
+        self.assertTrue("label" in output_df.columns)
+        self.assertTrue(len(output_df.index) == 3)
