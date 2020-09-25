b7ba2644cd79c71886ae4bb7d7acfede6862f139
lhenne <lhenne@posteo.de>
2020-09-25 11:50:09 +0200

    add nucleus label and duration assigment

--- promdetect/prep/find_syllable_nuclei.py
+++ promdetect/prep/find_syllable_nuclei.py
@@ -17,6 +17,7 @@ Import necessary packages:
 `io` for a parseable StringIO object to forward to pandas functions
 `glob` to collect all recordings that are to be processed
 """
+from pandas import DataFrame
 
 SAMPA_VOWELS = [
     "a:",
@@ -50,6 +51,42 @@ SAMPA_VOWELS = [
 EXTRALING_SOUNDS = ["[@]", "[t]", "[n]", "[f]", "[h]", "<P>"]
 
 
+def assign_points_labels(nuclei, phones, words):
+    """
+    This function assigns phone and word labels, as well as phone boundaries and the corresponding durations.
+    """
+
+    phones_filtered = filter_labels(phones, "phones")
+    words_filtered = filter_labels(words, "words")
+
+    assigned_df = DataFrame(
+        columns=["nucl_time", "phone", "word", "start_est", "end", "duration_est"]
+    )
+
+    assigned_df["nucl_time"] = nuclei
+
+    for row in assigned_df.itertuples():
+        match_phones = phones_filtered.loc[
+            (phones_filtered["start_est"] <= row.nucl_time)
+            & (phones_filtered["end"] >= row.nucl_time)
+        ].to_numpy()
+        match_words = words_filtered.loc[
+            (words_filtered["start_est"] <= row.nucl_time)
+            & (words_filtered["end"] >= row.nucl_time)
+        ]["label"].array
+
+        if match_phones.any():
+            assigned_df.at[row.Index, ["end", "phone", "start_est"]] = match_phones[0]
+
+        if match_words.any():
+            assigned_df.at[row.Index, "word"] = match_words[0]
+
+        assigned_df["duration_est"] = assigned_df["end"] - assigned_df["start_est"]
+        assigned_df.round({"duration_est": 4})
+
+    return assigned_df
+
+
 def filter_labels(annotation_df, annotation_type):
     """
     This function filters dataframes containing word or phone labels, dropping rows containing non-vowel or extralinguistic labels, respectively.
@@ -57,11 +94,11 @@ def filter_labels(annotation_df, annotation_type):
 
     if annotation_type == "phones":
         label_inv = SAMPA_VOWELS
-        keep_if = True
+        keep_if = True  # keep the rows where elements of SAMPA_VOWELS are found
 
     elif annotation_type == "words":
         label_inv = EXTRALING_SOUNDS
-        keep_if = False
+        keep_if = False  # drop the rows where elements of EXTRALING_SOUNDS are found
 
     else:
         raise ValueError("Annotation type '{}' not supported.".format(annotation_type))
