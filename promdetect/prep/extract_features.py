e088b9a26302f74a35ec31b4fa2b90cdb7ec16ac
lhenne <lhenne@posteo.de>
2020-10-01 11:43:13 +0200

    move checks for input columns to separate function

--- promdetect/prep/extract_features.py
+++ promdetect/prep/extract_features.py
@@ -14,12 +14,7 @@ def get_rms(snd_obj, nuclei):
     This function extracts the RMS value for syllable nuclei.
     """
 
-    if all(col in nuclei.columns for col in ["start_est", "end"]):
-        pass
-    else:
-        raise ValueError(
-            "Input must be of type DataFrame and contain 'start_est' and 'end' columns."
-        )
+    check_input_df(nuclei, ["start_est", "end"])
 
     rms_vals = np.array(
         [
@@ -29,3 +24,16 @@ def get_rms(snd_obj, nuclei):
     )
 
     return rms_vals
+
+
+# ANCILLARY FUNCTIONS
+def check_input_df(input_df, expected_cols):
+
+    if all(col in input_df.columns for col in expected_cols):
+        pass
+    else:
+        raise ValueError(
+            "Input must be of type DataFrame and contain the following columns: {}".format(
+                ", ".join(expected_cols)
+            )
+        )
