640ef8297f3922bb225fab483cf162d25476528d
lhenne <lhenne@posteo.de>
2020-10-04 00:16:44 +0200

    add normalized pitch excursion extraction

--- promdetect/prep/extract_features.py
+++ promdetect/prep/extract_features.py
@@ -11,6 +11,23 @@ import pandas as pd
 from parselmouth import praat
 
 
+# ANCILLARY FUNCTIONS
+def check_input_df(input_df, expected_cols):
+    """
+    Check if the input DataFrame contains all the necessary columns for the extraction.
+    """
+
+    if all(col in input_df.columns for col in expected_cols):
+        pass
+    else:
+        raise ValueError(
+            "Input must be of type DataFrame and contain the following columns: {}".format(
+                ", ".join(expected_cols)
+            )
+        )
+
+
+# EXTRACTION FUNCTIONS
 def get_rms(snd_obj, nuclei):
     """
     This function extracts the RMS value for syllable nuclei.
@@ -91,7 +108,9 @@ def get_f0_nuclei(pitch_obj, nuclei):
 
     f0_max = np.array(
         [
-            praat.call(pitch_obj, "Get maximum", row.start_est, row.end, "Hertz", "None")
+            praat.call(
+                pitch_obj, "Get maximum", row.start_est, row.end, "Hertz", "None"
+            )
             for row in nuclei.itertuples()
         ]
     )
@@ -99,17 +118,44 @@ def get_f0_nuclei(pitch_obj, nuclei):
     return f0_max
 
 
-# ANCILLARY FUNCTIONS
-def check_input_df(input_df, expected_cols):
+def get_excursion(pitch_obj, nuclei, level):
     """
-    Check if the input DataFrame contains all the necessary columns for the extraction.
+    This function extracts the pitch excursion with normalization on either the "word" level or the intonation phrase ("ip") level
     """
 
-    if all(col in input_df.columns for col in expected_cols):
-        pass
-    else:
-        raise ValueError(
-            "Input must be of type DataFrame and contain the following columns: {}".format(
-                ", ".join(expected_cols)
+    if level == "word":
+        check_input_df(nuclei, ["word_start", "word_end", "f0_max"])
+
+        timestamps = nuclei[["word_start", "word_end"]].drop_duplicates()
+
+        timestamps["f0_q10"] = [
+            praat.call(
+                pitch_obj, "Get quantile", row.word_start, row.word_end, 0.1, "Hertz"
             )
+            for row in timestamps.itertuples()
+        ]
+
+        norm_df = pd.merge(
+            nuclei, timestamps, on=["word_start", "word_end"], how="left"
         )
+
+    elif level == "ip":
+        check_input_df(nuclei, ["ip_start", "ip_end", "f0_max"])
+
+        timestamps = nuclei[["ip_start", "ip_end"]].drop_duplicates()
+
+        timestamps["f0_q10"] = [
+            praat.call(
+                pitch_obj, "Get quantile", row.ip_start, row.ip_end, 0.1, "Hertz"
+            )
+            for row in timestamps.itertuples()
+        ]
+
+        norm_df = pd.merge(nuclei, timestamps, on=["ip_start", "ip_end"], how="left")
+
+    else:
+        raise ValueError("Argument 'level' must be one of ['word', 'ip']")
+
+    excursions = np.array(12 * np.log2(norm_df["f0_max"] / norm_df["f0_q10"]))
+
+    return excursions
