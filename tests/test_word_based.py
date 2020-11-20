fd415ee8c6098bd34b69ed96289a95115f232607
lhenne <lhenne@posteo.de>
2020-11-20 15:05:05 +0100

    add intonation phrase tests

--- tests/test_word_based.py
+++ tests/test_word_based.py
@@ -54,3 +54,56 @@ class WordSegmentationTests(unittest.TestCase):
         self.assertTrue(ex_vals["start_frame"] == (ex_vals["start"] * 48_000))
         self.assertTrue(ex_vals["end_frame"] == (ex_vals["end"] * 48_000))
         self.assertTrue(ex_vals["duration_frames"] == (ex_vals["duration"] * 48_000))
+
+
+class IPSegmentationTests(unittest.TestCase):
+    def test_include_annotations(self):
+        tester = segmentation.Segmenter(
+            "tones", "/home/lukas/Dokumente/Uni/ma_thesis/quelldaten/DIRNDL-prosody"
+        )
+
+        tester.read_annotations()
+
+        self.assertTrue("dlf-nachrichten-200703250000" in tester.annotations.keys())
+
+    def test_output_type(self):
+        tester = segmentation.Segmenter(
+            "tones", "/home/lukas/Dokumente/Uni/ma_thesis/quelldaten/DIRNDL-prosody"
+        )
+
+        tester.read_annotations()
+
+        self.assertTrue(isinstance(tester.annotations, dict))
+        self.assertTrue(
+            isinstance(tester.annotations["dlf-nachrichten-200703250000"], pd.DataFrame)
+        )
+
+    def test_frame_calculation_output_format(self):
+        tester = segmentation.Segmenter(
+            "tones", "/home/lukas/Dokumente/Uni/ma_thesis/quelldaten/DIRNDL-prosody"
+        )
+
+        tester.read_annotations()
+        tester.add_frame_info()
+
+        ex_df = tester.annotations["dlf-nachrichten-200703250000"]
+
+        self.assertTrue("start_frame" in ex_df.columns)
+        self.assertTrue("end_frame" in ex_df.columns)
+        self.assertTrue("duration_frames" in ex_df.columns)
+
+    def test_frame_calculation_output_values(self):
+        tester = segmentation.Segmenter(
+            "tones", "/home/lukas/Dokumente/Uni/ma_thesis/quelldaten/DIRNDL-prosody"
+        )
+
+        tester.read_annotations()
+        tester.add_frame_info()
+
+        ex_df = tester.annotations["dlf-nachrichten-200703250000"]
+
+        ex_vals = ex_df.loc[42]
+
+        self.assertTrue(ex_vals["start_frame"] == (ex_vals["start"] * 48_000))
+        self.assertTrue(ex_vals["end_frame"] == (ex_vals["end"] * 48_000))
+        self.assertTrue(ex_vals["duration_frames"] == (ex_vals["duration"] * 48_000))
