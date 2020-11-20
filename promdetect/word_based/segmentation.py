70ddbc5c02efdd7fb1c4c00f615f369e5f946442
lhenne <lhenne@posteo.de>
2020-11-20 09:06:20 +0100

    set up segmentation script for word-based approach

---
+++ promdetect/word_based/segmentation.py
@@ -0,0 +1,13 @@
+class Segmenter:
+    def __init__(self, level, directory):
+        self.level = level
+        self.directory = directory
+
+    def read_annotations(self):
+        pass
+
+    def get_boundaries(self):
+        pass
+
+    def save_output(self, file):
+        pass
