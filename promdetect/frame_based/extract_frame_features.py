11c44baa581631583185c2edad7ee9b0b008b573
lhenne <lhenne@posteo.de>
2020-12-07 20:51:11 +0100

    add frame-based extraction calls

--- promdetect/frame_based/extract_frame_features.py
+++ promdetect/frame_based/extract_frame_features.py
@@ -1,6 +1,66 @@
 import pandas as pd
 import parselmouth as pm
 from parselmouth import praat
+from pathlib import Path
+from glob import glob
+
+SPEAKERS = {
+    "dlf-nachrichten-200703250000": "m",
+    "dlf-nachrichten-200703250100": "m",
+    "dlf-nachrichten-200703250300": "m",
+    "dlf-nachrichten-200703250400": "m",
+    "dlf-nachrichten-200703250500": "m",
+    "dlf-nachrichten-200703250600": "f",
+    "dlf-nachrichten-200703250700": "f",
+    "dlf-nachrichten-200703250800": "f",
+    "dlf-nachrichten-200703250900": "f",
+    "dlf-nachrichten-200703251000": "f",
+    "dlf-nachrichten-200703251100": "f",
+    "dlf-nachrichten-200703251200": "f",
+    "dlf-nachrichten-200703251300": "f",
+    "dlf-nachrichten-200703251400": "f",
+    "dlf-nachrichten-200703251500": "f",
+    "dlf-nachrichten-200703260000": "m",
+    "dlf-nachrichten-200703260100": "m",
+    "dlf-nachrichten-200703260200": "m",
+    "dlf-nachrichten-200703260300": "m",
+    "dlf-nachrichten-200703260400": "m",
+    "dlf-nachrichten-200703260500": "m",
+    "dlf-nachrichten-200703260600": "f",
+    "dlf-nachrichten-200703260700": "f",
+    "dlf-nachrichten-200703260800": "f",
+    "dlf-nachrichten-200703260900": "f",
+    "dlf-nachrichten-200703261000": "f",
+    "dlf-nachrichten-200703261100": "f",
+    "dlf-nachrichten-200703261200": "f",
+    "dlf-nachrichten-200703261300": "f",
+    "dlf-nachrichten-200703261400": "f",
+    "dlf-nachrichten-200703261500": "f",
+    "dlf-nachrichten-200703261600": "f",
+    "dlf-nachrichten-200703261700": "f",
+    "dlf-nachrichten-200703261800": "m",
+    "dlf-nachrichten-200703261900": "m",
+    "dlf-nachrichten-200703262000": "m",
+    "dlf-nachrichten-200703262100": "m",
+    "dlf-nachrichten-200703262200": "m",
+    "dlf-nachrichten-200703262300": "m",
+    "dlf-nachrichten-200703270000": "m",
+    "dlf-nachrichten-200703270100": "m",
+    "dlf-nachrichten-200703270200": "m",
+    "dlf-nachrichten-200703270300": "m",
+    "dlf-nachrichten-200703270400": "m",
+    "dlf-nachrichten-200703270500": "m",
+    "dlf-nachrichten-200703270600": "m",
+    "dlf-nachrichten-200703270700": "m",
+    "dlf-nachrichten-200703270800": "m",
+    "dlf-nachrichten-200703270900": "m",
+    "dlf-nachrichten-200703271000": "m",
+    "dlf-nachrichten-200703271100": "m",
+    "dlf-nachrichten-200703271200": "m",
+    "dlf-nachrichten-200703271300": "m",
+    "dlf-nachrichten-200703271400": "m",
+    "dlf-nachrichten-200703271500": "m",
+}
 
 
 class FrameLevelExtractor:
@@ -9,6 +69,7 @@ class FrameLevelExtractor:
         self.snd_obj = pm.Sound(self.wav_file)
         self.gender = gender
         self.features = pd.DataFrame()
+        self.path = f"features/{wav_file}.frames"
 
         if gender == "f":
             self.__pitch_range = (75, 500)
@@ -62,7 +123,7 @@ class FrameLevelExtractor:
             for frame in self.features.itertuples()
         ]
 
-        self.features.drop(columns="excitation")
+        self.features = self.features.drop(columns="excitation")
 
     def zcr_extraction(self):
         self.features["snd_part"] = [
@@ -80,7 +141,7 @@ class FrameLevelExtractor:
             for frame in self.features.itertuples()
         ]
 
-        self.features.drop(columns=["snd_part", "pp_part"])
+        self.features = self.features.drop(columns=["snd_part", "pp_part"])
 
     def hnr_extraction(self):
         self.harm_obj = self.snd_obj.to_harmonicity_cc(
@@ -91,3 +152,18 @@ class FrameLevelExtractor:
             praat.call(self.harm_obj, "Get value in frame", frame.Index)
             for frame in self.features.itertuples()
         ]
+
+    def write_features(self):
+        self.features.to_csv(self.path)
+
+
+for wav in glob("*.wav"):
+    recording = Path(wav).stem
+    gender = SPEAKERS[recording]
+
+    extractor = FrameLevelExtractor(wav, gender)
+    extractor.rms_extraction()
+    extractor.loudness_extraction()
+    extractor.zcr_extraction()
+    extractor.hnr_extraction()
+    extractor.write_features()
